5f9f0319b62c82ece2f4670baf56976fef5d29ef
unknown <vannp01@myunitec.ac.nz>
2020-10-20 17:47:34 +1300

    tweaked FusedLocationService code

--- Assignment2-MobileApp-By-PonhvathVann-1502538/app/src/main/java/com/example/assignment2_ponhvathvann_1502538/FusedLocationServiceExercise.java
+++ Assignment2-MobileApp-By-PonhvathVann-1502538/app/src/main/java/com/example/assignment2_ponhvathvann_1502538/FusedLocationServiceExercise.java
@@ -1,5 +1,7 @@
 package com.example.assignment2_ponhvathvann_1502538;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -35,22 +39,37 @@ public class FusedLocationServiceExercise extends AppCompatActivity {
         btnGetCurrentLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    Task<Location> currentLocation = client.getLastLocation();
+                if (ContextCompat.checkSelfPermission(FusedLocationServiceExercise.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted? Ask for permission
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            FusedLocationServiceExercise.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        // Show an explanation asynchronously and ask for permission
+                    } else { // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(
+                                FusedLocationServiceExercise.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                REQUEST_LOCATION);
+                    }
+                } else { // Permission has already been granted
+                    try {
+                        Task<Location> currentLocation = client.getLastLocation();
 
-                    currentLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Location> task) {
-                            String latitude = Double.toString(task.getResult().getLatitude());
-                            String longitude = Double.toString(task.getResult().getLongitude());
+                        currentLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                String latitude = Double.toString(task.getResult().getLatitude());
+                                String longitude = Double.toString(task.getResult().getLongitude());
 
-                            txtViewLatitude.setText("Current Latitude: " + latitude);
-                            txtViewLongitude.setText("Current Longitude: " + longitude);
-                        }
-                    });
-
-                }catch (SecurityException ex) {
-                    ex.printStackTrace();
+                                txtViewLatitude.setText("Current Latitude: " + latitude);
+                                txtViewLongitude.setText("Current Longitude: " + longitude);
+                            }
+                        });
+                    }catch (SecurityException ex) {
+                        ex.printStackTrace();
+                    }
                 }
             }
         });
