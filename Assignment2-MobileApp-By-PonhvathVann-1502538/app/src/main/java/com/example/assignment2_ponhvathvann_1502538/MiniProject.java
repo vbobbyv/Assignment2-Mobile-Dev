50d897e03ed797f17a1e1d5ff239545e643375bf
unknown <vannp01@myunitec.ac.nz>
2020-10-21 16:07:31 +1300

    implemented Places API to search for location

--- Assignment2-MobileApp-By-PonhvathVann-1502538/app/src/main/java/com/example/assignment2_ponhvathvann_1502538/MiniProject.java
+++ Assignment2-MobileApp-By-PonhvathVann-1502538/app/src/main/java/com/example/assignment2_ponhvathvann_1502538/MiniProject.java
@@ -1,24 +1,90 @@
 package com.example.assignment2_ponhvathvann_1502538;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-public class MiniProject extends FragmentActivity implements OnMapReadyCallback {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+public class MiniProject extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
     private GoogleMap mMap;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 1 ;
+    private FusedLocationProviderClient client;
+    private boolean requestLocation = false;
+    AutocompleteSupportFragment autocompleteFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mini_project);
+
+        autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyCCvZDYG9_zmCFqsXqDXM1zXIIf0DYnNFg");
+        }
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NotNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("Search", "Place: " + place.getName() + ", " + place.getId());
+                System.out.println(place.getAddress());
+                System.out.println(place.getName());
+                System.out.println(place.getLatLng());
+
+                LatLng latLng = place.getLatLng();
+                String locationName = place.getName();
+                String address = place.getAddress();
+
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(latLng).title(locationName));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            }
+
+
+            @Override
+            public void onError(@NotNull Status status) {
+                // TODO: Handle the error.
+                Log.i("Search", "An error occurred: " + status);
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -37,10 +103,90 @@ public class MiniProject extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.clear();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Auckland and move the camera
+        LatLng auckland = new LatLng(-36.848461, 174.763336);
+        mMap.addMarker(new MarkerOptions().position(auckland).title("Marker in Auckland"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(auckland));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted? Ask for permission
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation asynchronously and ask for permission
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+            }
+        } else {
+            // Permission has already been granted, Do something else
+            try{
+                LocationRequest req = new LocationRequest();
+//                req.setInterval(2000); // 2 seconds
+//                req.setFastestInterval(500); // 500 milliseconds
+//                req.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+                client.requestLocationUpdates(req, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        mMap.clear();
+                        Location location = locationResult.getLastLocation();
+
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                        Marker myLocation = mMap.addMarker(new MarkerOptions().position(latLng));
+
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        CameraPosition cameraPosition = new CameraPosition.Builder()
+                                .target(latLng)
+                                .zoom(15)
+                                .build();
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                        mMap.setMyLocationEnabled(true);
+                        mMap.setOnMyLocationButtonClickListener(MiniProject.this);
+                        mMap.setOnMyLocationClickListener(MiniProject.this);
+
+                    }
+                }, null);
+
+            }catch (SecurityException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    requestLocation = true;
+                } else {
+                    requestLocation = false;
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
     }
 }
